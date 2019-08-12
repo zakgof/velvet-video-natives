08dc69b6f6ff46b41c3f46d757d657a26b46ffd4
Alexander Zakusylo <zakgof@gmail.com>
2019-08-12 17:27:50 +0300

    Building openh264, work in progress

--- travis/do-docker-build.bat
+++ travis/do-docker-build.bat
@@ -3,9 +3,9 @@ mkdir %~dp0\output
 
 docker build - -t ffmpeg-prepare < Dockerfile-prepare
 docker build - -t ffmpeg-windows < Dockerfile-windows
-docker build - -t ffmpeg-linux < Dockerfile-linux
+REM docker build - -t ffmpeg-linux < Dockerfile-linux
 
 docker run -v %~dp0\output:/output --name ffmpeg-win --rm -it ffmpeg-windows
-docker run -v %~dp0\output:/output --name ffmpeg-lin --rm -it ffmpeg-linux
+REM docker run -v %~dp0\output:/output --name ffmpeg-lin --rm -it ffmpeg-linux
    
    
